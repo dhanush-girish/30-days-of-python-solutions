ac7f5fa90b6aa1bdd925431b13ddf084a7941416
Dhanush Girish <mail.dhanushgirish@gmail.com>
2026-06-10 08:09:52 +0530

    day 9 added

--- day_9/conditionals.py
+++ day_9/conditionals.py
@@ -1,3 +1,11 @@
 # Day 9 - 30DaysOfPython Challenge
 # Conditionals
 
+#Exercises: Level 1
+#question 1
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are old enough to drive.")
+else:
+    years_left = 18 - age
+    print(f"You need {years_left} more years to learn to drive.")
