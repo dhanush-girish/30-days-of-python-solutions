a1d52b19f6dff1765bdf480b15b578b05b9fbcb8
Dhanush Girish <mail.dhanushgirish@gmail.com>
2026-06-26 22:00:41 +0530

    day 10 added

---
+++ day_10/Loops.py
@@ -0,0 +1,12 @@
+# Day 10 - 30DaysOfPython Challenge
+# Loops
+
+#Exercises: Level 1
+#question 1
+for i in range(11):
+    print(i)
+
+num = range(1,11)
+while num == 11:
+    print(num)
+    num += 1
---
+++ day_10/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+num = range(1,11)
+while num == 11:
+    print(num)
+    num += 1
